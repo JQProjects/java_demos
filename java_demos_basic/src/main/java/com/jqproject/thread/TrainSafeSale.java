559b0fc1df09283b51409dd92be514651bc708a8
JQBase <309158177@qq.com>
2020-02-04 17:44:31 +0800

    新增一些多线程的示意

---
+++ java_demos_basic/src/main/java/com/jqproject/thread/TrainSafeSale.java
@@ -0,0 +1,14 @@
+package com.jqproject.thread;
+
+/**
+ * @author 姜庆
+ * @create 2020-02-04 17:39
+ * @desc 启用synchronize锁的机制来保证安全性
+ **/
+public class TrainSafeSale implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ java_demos_basic/src/main/java/com/jqproject/thread/TrainSale.java
@@ -0,0 +1,37 @@
+package com.jqproject.thread;
+
+/**
+ * @author 姜庆
+ * @create 2020-02-04 17:23
+ * @desc 通过售票来演示多线程的安全问题
+ **/
+public class TrainSale implements Runnable {
+
+    /**
+     * 对于全局变量的竞争使用导致了多线程的不安全
+     */
+    public static long totalCount = 100;
+
+    @Override
+    public void run() {
+        while (totalCount > 0) {
+
+            System.out.println(Thread.currentThread().getName() + ",现在售卖第" + (100 - totalCount + 1) + "张票");
+            totalCount--;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        TrainSale t1 = new TrainSale();
+        TrainSale t2 = new TrainSale();
+
+        Thread thread1 = new Thread(t1, "窗口1");
+        Thread thread2 = new Thread(t2, "窗口2");
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
